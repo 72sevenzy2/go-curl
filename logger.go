0860d4488b2cc3f6718d214d7b62c4aae069a1df
72 <72sevenzy2@gmail.com>
2026-05-14 21:22:16 +0500

    feat: include request method

--- logger.go
+++ logger.go
@@ -15,10 +15,12 @@ func Log(v *http.Client, req *http.Request) (time.Duration, *http.Response, erro
 	end := time.Since(start)
 
 	fmt.Println("visited to:", req.URL.Path)
+	fmt.Println("method:", req.Method)
 	// exlude sensitive headers
 	newHeaders := req.Header.Clone()
 	newHeaders.Del("Authorization")
 	fmt.Println("with headers:", newHeaders) // then display
+
 
 	return end, resp, nil
 }
