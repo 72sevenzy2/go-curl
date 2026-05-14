206b7963ae0b8327eae09e6a5e8f5bc329391287
72 <72sevenzy2@gmail.com>
2026-05-14 19:47:48 +0500

    feat: add url path tracker + header showcasing (without sensitive headers (Auth))

--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -8,11 +9,16 @@ import (
 func Log(v *http.Client, req *http.Request) (time.Duration, *http.Response, error) {
 	start := time.Now()
 	resp, err := v.Do(req)
-	end := time.Since(start)
-
 	if err != nil {
 		return 0, nil, err
 	}
+	end := time.Since(start)
+
+	fmt.Println("visited to:", req.URL.Path)
+	// exlude sensitive headers
+	newHeaders := req.Header.Clone()
+	newHeaders.Del("Authorization")
+	fmt.Println("with headers:", newHeaders) // then display
 
 	return end, resp, nil
 }
