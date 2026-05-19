a2dbd9dcda7282d2a408b7910c776488343cd495
72 <72sevenzy2@gmail.com>
2026-05-19 16:15:45 +0500

    fix: -s flag not parsing (changed Validate() func) from 'if len(args) < 2' to 'if len(args) < 1'.

--- utils.go
+++ utils.go
@@ -17,7 +17,7 @@ func (h *HeaderFlags) Set(value string) error {
 }
 
 func Validate(args []string) error {
-	if len(args) < 2 {
+	if len(args) < 1 {
 		UsageMsg := errors.New("usage > main.go <URL> [-H key:value]")
 		return fmt.Errorf("%s", UsageMsg.Error())
 	}
