77403da5f9c82e746d428dcbc1012039fa260336
72 <72sevenzy2@gmail.com>
2026-05-20 15:29:22 +0500

    refactor: simplify test modules (remove tests.go and moved logic to storage_test.go).

--- storage_test.go
+++ storage_test.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestStorage(t *testing.T) {
-	TestGet()
+	store := NewStore()
+	_, ok := store.Set(1, "test")
+	if ok {
+		fmt.Println("data set successfully")
+	} else {
+		fmt.Println("could not save data")
+	}
+
+	val, exists, _ := store.Get(1)
+	if exists {
+		fmt.Println("data exists:", val)
+	} else {
+		fmt.Println("couldnt retrieve data")
+	}
 }
