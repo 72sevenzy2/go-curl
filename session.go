ab6294d6c9ea6c1eaa6c1c503b52ce4bbdd3de15
72 <72sevenzy2@gmail.com>
2026-05-20 16:59:41 +0500

    feat/test: finished intial version + tested (failing).

--- session.go
+++ session.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/http"
 	"strings"
 )
 
@@ -23,6 +24,7 @@ func StartSession(b *bufio.Scanner, store *Data) {
 		upperInput := strings.ToUpper(parts[0]) // "VAR", "GET", "DEL", "EXIT"
 
 		switch upperInput {
+		// declare variables (can be to store headers, urls, etc)
 		case "VAR":
 			err, ok := store.Set(parts[1], parts[2])
 			if err != nil && !ok {
@@ -46,6 +48,40 @@ func StartSession(b *bufio.Scanner, store *Data) {
 				fmt.Println(err.Error())
 				continue
 			}
+
+		// actual api testing logic (GET only for now)
+		case "TEST":
+			if parts[1] == "" { // parts[1] will be the url the user wanted to test
+				fmt.Println("please include a valid url aswell.")
+				continue
+			} else {
+				client := http.Client{}
+				cl, err := http.NewRequest(http.MethodGet, parts[1], nil) // new request
+				if err != nil {
+					fmt.Println(err.Error())
+					continue
+				}
+ 
+				resp, err2 := client.Do(cl) // send the request to the url provided
+				if err2 == nil {
+					// shadow auth header from resp
+					clonedH := resp.Header.Clone()
+					clonedH.Del("authorization")
+
+					respB := resp.Body
+					defer respB.Close() // close the connection at the end of this block
+
+					// outputting
+					fmt.Println("response headers:")
+					fmt.Println(clonedH)
+					for range 10 {
+						fmt.Println("-") // seperator for headers and resp body so its easier to read
+					}
+					fmt.Println("response body:")
+					fmt.Println(respB)
+				}
+			}
+		// for exiting
 		case "EXIT":
 			fmt.Println("exiting will remove saved variables.")
 			return
